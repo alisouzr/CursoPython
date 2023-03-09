d3053fd495d9f88ac9670a25fef0c73f9d445248
Aliny <aliny.souz.r@gmail.com>
2023-03-09 15:19:29 -0300

    HackerRank Exercício Day2 'Diagonal Difference'

---
+++ hackerrank/diagonalDifference.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    somaL = 0
+    somaR = 0
+    for x in range(0, len(arr)):
+        somaL += arr[x][x]
+    for y in range(0, len(arr)):
+        somaR += arr[y][len(arr)-1-y]
+    return abs(somaL-somaR)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
